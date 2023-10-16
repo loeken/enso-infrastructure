db1b1b7ead5a65c55b8f1f897209915a5728aa9b
loeken <loeken@internetz.me>
2023-10-17 01:13:09 +0200

    testing

--- ansible/proxmox/roles/opnsense/files/setup.sh
+++ ansible/proxmox/roles/opnsense/files/setup.sh
@@ -2,7 +2,7 @@
 fetch -o /usr/local/etc/pkg/repos/mimugmail.conf https://www.routerperformance.net/mimugmail.conf
 pkg update
 pkg upgrade -y
-umount /mnt/cd0
+umount /mnt/cdrom
 opnsense-importer cd0
 pkg install -y os-adguardhome-maxit os-sensei os-sensei-updater os-sunnyvalley os-theme-rebellion
 reboot
