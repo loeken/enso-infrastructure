848df51218869dd8f04f89657db5841dd25a191c
loeken <loeken@internetz.me>
2023-10-17 01:04:17 +0200

    testing

--- ansible/proxmox/roles/opnsense/files/setup.sh
+++ ansible/proxmox/roles/opnsense/files/setup.sh
@@ -2,6 +2,7 @@
 fetch -o /usr/local/etc/pkg/repos/mimugmail.conf https://www.routerperformance.net/mimugmail.conf
 pkg update
 pkg upgrade -y
-pkg install -y os-adguardhome-maxit os-sensei os-sensei-updater os-sunnyvalley os-theme-rebellion
+umount /mnt/cd0
 opnsense-importer cd0
+pkg install -y os-adguardhome-maxit os-sensei os-sensei-updater os-sunnyvalley os-theme-rebellion
 reboot
