0c80954065c68b12a241fa9fa4c4af2a569c5916
loeken <loeken@internetz.me>
2023-10-17 20:24:13 +0200

    testing

--- ansible/proxmox/roles/opnsense/files/setup.sh
+++ ansible/proxmox/roles/opnsense/files/setup.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-
-# Check if script is running from /mnt/cdrom/setup.sh
-if [ "$0" != "/root/setup.sh" ]; then
-  cp /mnt/cdrom/setup.sh /root/setup.sh
-  chmod +x /root/setup.sh
-  exec /root/setup.sh
-  exit 0
-fi
-
 # Your original script
 echo 'mimugmail: {
   url: "https://opn-repo.routerperformance.net/repo/${ABI}",
@@ -20,8 +11,6 @@ pkg upgrade -y
 pkg install -y os-sunnyvalley nano
 pkg install -y os-adguardhome-maxit os-sensei os-sensei-updater os-theme-rebellion elasticsearch5
 
-umount /dev/cd0
-sleep 3
 opnsense-importer cd0
 touch /.probe.for.growfs
 echo "reboot now, if you want to increase the disk change the disk size before rebooting"
