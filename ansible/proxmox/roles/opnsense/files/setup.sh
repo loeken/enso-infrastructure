ef77b9a4aee2b422355330a722b121355d7dbab0
loeken <loeken@internetz.me>
2023-10-18 21:31:58 +0200

    testing

--- ansible/proxmox/roles/opnsense/files/setup.sh
+++ ansible/proxmox/roles/opnsense/files/setup.sh
@@ -14,6 +14,8 @@ echo 'mimugmail: {
   priority: 190,
   enabled: yes
 }' > /usr/local/etc/pkg/repos/mimugmail.conf
+
+
 # Path to the OPNsense config file
 CONFIG_FILE="/conf/config.xml"
 
@@ -22,6 +24,21 @@ sed -i -e '/<wan>/,/<\/wan>/ s|<if>.*</if>|<if>vtnet0</if>|' $CONFIG_FILE
 
 # Set vtnet1 for LAN
 sed -i -e '/<lan>/,/<\/lan>/ s|<if>.*</if>|<if>vtnet1</if>|' $CONFIG_FILE
+
+# Replace 'vtnet0' with your interface name
+INTERFACE="vtnet1"
+
+# Get all IPv6 addresses on the interface
+ADDRESSES=$(ifconfig ${INTERFACE} | grep inet6 | awk '{ print $2}')
+
+# Loop through each address and remove it
+for addr in ${ADDRESSES}; do
+  ifconfig ${INTERFACE} inet6 ${addr} delete
+done
+
+sysctl net.inet6.ip6.accept_rtadv=0
+sysctl net.inet6.ip6.forwarding=0
+sysctl net.inet6.ip6.disable_ipv6=1
 
 # Reload all configurations (you may choose to execute this step manually)
 /usr/local/etc/rc.reload_all
