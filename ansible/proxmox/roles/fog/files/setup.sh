d7046988b2697e4bd6516988c3613d4a4da221a7
loeken <loeken@internetz.me>
2023-10-18 13:39:30 +0200

    testing

--- ansible/proxmox/roles/fog/files/setup.sh
+++ ansible/proxmox/roles/fog/files/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install git
+apt install git -y
 git clone https://github.com/fogproject/fogproject.git fogproject-master
 cd fogproject-master/bin
-./installfog.sh --autoaccept=yes
+./installfog.sh -Y
