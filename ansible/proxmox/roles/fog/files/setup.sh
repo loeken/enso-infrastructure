a332106ae71c2b93466da75c63f783fe93a928a8
loeken <loeken@internetz.me>
2023-10-19 04:08:48 +0200

    logging for install

--- ansible/proxmox/roles/fog/files/setup.sh
+++ ansible/proxmox/roles/fog/files/setup.sh
@@ -6,4 +6,4 @@ cd /tmp
 git clone https://github.com/fogproject/fogproject.git fogproject-master
 cd /tmp/fogproject-master/bin/
 pwd
-/bin/bash installfog.sh -Y
+/bin/bash installfog.sh -Y > /tmp/fog_install.log
