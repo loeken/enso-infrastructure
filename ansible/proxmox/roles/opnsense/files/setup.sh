0191f003f156c757361f8973b836295ae30997f3
loeken <loeken@internetz.me>
2023-11-16 17:09:10 +0000

    temp testing

--- ansible/proxmox/roles/opnsense/files/setup.sh
+++ ansible/proxmox/roles/opnsense/files/setup.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
 
-# Check if script is running from /mnt/cdrom/setup.sh
-if [ "$0" != "/root/setup.sh" ]; then
-  cp /mnt/cdrom/setup.sh /root/setup.sh
-  chmod +x /root/setup.sh
-  exec /root/setup.sh
-  exit 0
-fi
-
-# Your original script
 echo 'mimugmail: {
   url: "https://opn-repo.routerperformance.net/repo/${ABI}",
   priority: 190,
   enabled: yes
 }' > /usr/local/etc/pkg/repos/mimugmail.conf
 
+cp /mnt/cdrom/conf/config.xml /conf/config.xml
+
+umount /dev/cd0
+sleep 5
+touch /.probe.for.growfs
+
 pkg update
 pkg upgrade -y
 pkg install -y os-sunnyvalley nano
 pkg install -y os-adguardhome-maxit os-sensei os-sensei-updater os-theme-rebellion
 
-umount /dev/cd0
-sleep 3
-opnsense-importer cd0 > /tmp/install.log
-touch /.probe.for.growfs
-
-echo reboot
+reboot
