c8efee97aa31c25cc0ff3f57f5922dd746683c71
loeken <loeken@internetz.me>
2024-02-12 20:21:18 +0100

    testing

--- terraform/k3s-baremetal/main.tf
+++ terraform/k3s-baremetal/main.tf
@@ -1,26 +1,23 @@
+variable "ssh_private_key_path" {
+  description = "Path to the SSH private key"
+  default     = "~/.ssh/id_ed25519"
+}
 resource "null_resource" "k3s_installation" {
-  count = length(var.vm_ips)
-
   connection {
     type        = "ssh"
     user        = var.user_name
     private_key = file(var.ssh_private_key_path)
-    host        = element(var.vm_ips, count.index)
+    host        = var.external_ip
+    port        = var.port
   }
 
   provisioner "remote-exec" {
     inline = [
-      count.index < 3 ? "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${var.kubernetes_version} sh -s - --write-kubeconfig-mode 644" :
-        "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${var.kubernetes_version} K3S_URL=https://${element(var.vm_ips, 0)}:6443 K3S_TOKEN=$(ssh ${var.user_name}@${element(var.vm_ips, 0)} 'sudo cat /var/lib/rancher/k3s/server/node-token') sh -s -",
+      "k3sup install --ip ${var.external_ip} --user ${var.user_name} --ssh-key ${var.ssh_private_key_path} --k3s-version ${var.k3s_version} --cluster",
     ]
   }
-
 }
 
-output "k3s_master_node" {
-  value = element(var.vm_ips, 0)
-}
-
-output "k3s_worker_nodes" {
-  value = slice(var.vm_ips, 1, length(var.vm_ips))
+output "k3s_cluster_info" {
+  value = "K3s cluster installed on node with IP: ${var.external_ip}"
 }
