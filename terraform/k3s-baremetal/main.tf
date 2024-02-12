81ffbd281437cc0399165a5306e8a419cf5ecd08
loeken <loeken@internetz.me>
2024-02-12 20:44:24 +0100

    testing

--- terraform/k3s-baremetal/main.tf
+++ terraform/k3s-baremetal/main.tf
@@ -1,45 +1,8 @@
-resource "null_resource" "k3sup_installation" {
-  connection {
-    type        = "ssh"
-    host        = var.external_ip
-    user        = var.user_name
-    private_key = file(var.ssh_private_key_path)
-    port        = var.port
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl -sLS https://get.k3sup.dev | sh",
-      "sudo install k3sup /usr/local/bin/"
-    ]
-  }
-}
 resource "null_resource" "k3s_installation" {
-  connection {
-    type        = "ssh"
-    user        = var.user_name
-    private_key = file(var.ssh_private_key_path)
-    host        = var.external_ip
-    port        = var.port
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "k3sup install --ip ${var.external_ip} --user ${var.user_name} --ssh-key ${var.ssh_private_key_path} --k3s-version ${var.kubernetes_version} --cluster",
-    ]
-  }
+  # Using local-exec to run k3sup install locally within the Docker container
   provisioner "local-exec" {
-    command = format("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P %s %s@%s:/home/%s/kubeconfig ./kubeconfig",
-      var.port,
-      var.user_name,
-      var.external_ip,
-      var.user_name
-    )
+    command = "k3sup install --ip ${var.external_ip} --user ${var.user_name} --ssh-key ${var.ssh_private_key_path} --k3s-version ${var.k3s_version} --cluster"
   }
-
-  depends_on = [
-    null_resource.k3sup_installation
-  ]
 }
 
 output "k3s_cluster_info" {
